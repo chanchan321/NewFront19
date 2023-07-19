c7329ea2a873a9e9ebb5f9ebd1d5fad2dd9c35f7
chanchan321 <116455671+chanchan321@users.noreply.github.com>
2023-07-19 14:43:48 +0800

    Update studeAddAppointment.js

--- src/Appointment/studeAddAppointment.js
+++ src/Appointment/studeAddAppointment.js
@@ -170,7 +170,9 @@ export default function StudeAddAppointment({haverequest,refresh,appointmentD,lo
         const getLongEventss = async () =>{
           try{
             const response= await Axios.get(`https://newback19.onrender.com/longEvents`,
-                                            credentials:'include')
+                                            {
+        withCredentials: true,
+      },')
             
              setselected((response.data).map((item)=> {
               return {
